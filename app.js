77983c15f9f2101b2134fd59b94e5a3688c2cb13
yash <yashguptace@gmail.com>
2022-11-22 17:39:52 +0530

    fix

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
+    await connectDB()
     app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,27 @@
 const mongoose = require('mongoose')
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useCreateIndex:true,
-    useFindAndModify:false,
-  })
+//const connectDB = (url) => {
+  //return mongoose.connect(url, {
+    //useNewUrlParser:true,
+    //useUnifiedTopology:true,
+    //useCreateIndex:true,
+    //useFindAndModify:false,
+  //})
+//}
+
+const connectDB = async ()=>{
+  try{
+      const conn = await mongoose.connect(process.env.MONGO_URI,{
+          useUnifiedTopology:true,
+          useNewUrlParser: true,
+          useCreateIndex: true,
+          useFindAndModify: true,
+      })
+      console.log(`MongoDB is connected! ${conn.connection.host}`)
+  }catch(error){
+      console.error(`Error: ${error}`)
+      process.exit(1) 
+  }
 }
 
 module.exports = connectDB
